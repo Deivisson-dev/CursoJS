426d71438892ba66b89e94849e581e6931da60ad
Deivisson-dev <darsonsilva83@gmail.com>
2023-05-23 14:14:41 -0300

    first commit

---
+++ aula04/script.js
@@ -0,0 +1,13 @@
+alert('Ola,Mundo!');
+
+var nome = prompt('Qual seu nome?');
+
+alert('Bem vindo ' + nome)
+
+var condicao =  prompt('Como você esta ' + nome)
+
+if (condicao == 'Estou Bem'){
+    alert('Tenha Um ótimo dia')
+} else{
+    alert('Que pena')
+}
