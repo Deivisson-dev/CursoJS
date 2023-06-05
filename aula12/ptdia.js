7a7c7643db6a9ae2eab71d0819e0c13048028e18
Deivisson-dev <darsonsilva83@gmail.com>
2023-06-05 11:01:54 -0300

    fix bugs

--- aula12/ptdia.js
+++ aula12/ptdia.js
@@ -7,13 +7,13 @@ const img = document.getElementById('imagem')
 if (hora >= 0 && hora < 12){
     const txt = document.getElementById('txt').innerHTML = `Agora são ${hora} horas`
     img.src = 'manha.jpg'
-    const body = document.querySelector("body").style.background = "darkgoldenrod"
+    const body = document.querySelector("body").style.background = "#e2cd9f"
 } else if (hora >= 12 && hora < 18){
     const txt = document.getElementById('txt').innerHTML = `Agora são ${hora} horas`
     img.src = 'tarde.jpg'
-    const body = document.querySelector("body").style.background = "coral"
+    const body = document.querySelector("body").style.background = "#b9846f"
 } else {
     const txt = document.getElementById('txt').innerHTML = `Agora são ${hora} horas`
     img.src = 'noite.jpg'
-    const body = document.querySelector("body").style.background = "gray"
+    const body = document.querySelector("body").style.background = "#515154"
 }
